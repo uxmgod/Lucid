3cf00b56e2cfa4b7862481f6975f218c302ba88c
Unknown <neenjapolygon@gmail.com>
2018-03-01 13:41:45 +1100

    Removed info command

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from discord.ext.commands import bot
 
 client = commands.Bot(command_prefix=prefix, self_bot=True)
 
-print("Attempting to loggin to Discord account...\n")
+print("Attempting to login to Discord account...\n")
 
 @client.event
 async def on_ready():
@@ -29,10 +29,5 @@ async def clear(ctx, limit: int=None):
                 pass
     em = discord.Embed(title='Lucid', description="Messages purged :crystal_ball:", color=0x00FFFF)
     await client.say (embed=em)
-
-async def info(ctx, limit: int=None):
-    if client.user.id == msg.author.id:
-        em = discord.Embed(title='Lucid', description="Created by Ninja#9525 @ https://github.com/neenjapolygon", color=0x00FFFF)
-        client.say(embed=em)
 
 client.run(token, bot=False)
